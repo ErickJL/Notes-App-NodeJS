4b78c461996974335db718e2a7f0417dffcfa7b9
Erick Lalihatu <erick.lalihatu@jatis.com>
2025-03-30 17:58:15 +0700

    add view for web server

--- Web-Server/index.js
+++ Web-Server/index.js
@@ -1,20 +1,30 @@
 import noteService from "./Services/JsonFile/noteService.js";
 import http from "http";
+import fs from "fs";
+import path from "path";
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
-
-  if (req.method === "GET" && req.url === "/notes") {
-    res.statusCode = 200;
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile('Views/index.html', (err, content) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end({message: 'Internal Server Error'});
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+} else if (req.method === "GET" && req.url === "/notes") {
+    res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(noteService.readAllNotes()));
   } else if (req.method === "GET" && req.url.startsWith("/notes")) {
     const title = decodeURIComponent(req.url.split('/')[2]);
         const item = noteService.readNote(title);
         if (item) {
-            res.statusCode = 200;
+            res.writeHead(200, { "Content-Type": "application/json" });
             res.end(JSON.stringify(item));
         } else {
-            res.statusCode = 404;
+            res.writeHead(200, { "Content-Type": "application/json" });
             res.end(JSON.stringify({ message: 'Note not found' }));
         }
   } else if (req.method === "POST" && req.url === "/notes") {
@@ -24,7 +34,7 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const flag = noteService.addNote(JSON.parse(body));
-      res.statusCode = 201;
+      res.writeHead(201, { "Content-Type": "application/json" });
       res.end(JSON.stringify({flag, message: "Note added" }));
     });
   } else if (req.method === "PUT" && req.url.startsWith("/notes/")) {
@@ -35,16 +45,16 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const flag = noteService.updateNote(title, JSON.parse(body));
-      res.statusCode = 200;
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({flag, message: "Note updated" }));
     });
   } else if (req.method === "DELETE" && req.url.startsWith("/notes/")) {
     const title = decodeURIComponent(req.url.split("/")[2]);
     const flag = noteService.removeNote(title);
-    res.statusCode = 200;
+    res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({flag, message: "Note deleted" }));
   } else {
-    res.statusCode = 404;
+    res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Not Found" }));
   }
 });
